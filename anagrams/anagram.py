41fbfd845341b37584660f725fa988204acea605
Akshay Kale <Akshay>
2018-03-18 11:59:00 -0500

    added anagram basic method

---
+++ anagrams/anagram.py
@@ -0,0 +1,33 @@
+import re
+
+## Check for anagrams 
+## Solving Anagrams using sort method
+def anagram(string1,  string2):
+    return cleaned(string1) == cleaned(string2)
+
+## Need to write a clean replace function.
+## Need to write alternate methods using dictionary.
+def cleaned(string):
+    ## replace special character with 
+    ## str.replace("!@#$%^&*()[]{};:,./<>?\|`~-=_+", " ")
+    ## to lower case string = string.lower()
+    ## to splitstring = str
+    ## to sort string = sorted(string)
+    ## to join string = "".join(string)
+    escapes = "!@#$%^&*()[]{};:,./<>?\|`~-=_+"
+    esc = [e for e in escapes]
+    for es in esc:
+        string = string.replace(es,"")
+    string = string.lower()
+    #string = string.split('')
+    string = sorted(string)
+    string = "".join(string)
+    print(string)
+    return string
+
+if __name__=="__main__":
+   string = "rail! safety!"
+   string1 = "fairy tales"
+   
+   print(anagram(string,string1))
+
