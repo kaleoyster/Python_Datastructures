fe578071b2f1e3c985d6d01a1ee94eedeb68c7ab
Akshay Kale <akshaykale915@gmail.com>
2018-03-22 21:33:21 -0500

    fixed intendation

--- reversed strings/rev_string.py
+++ reversed strings/rev_string.py
@@ -4,6 +4,7 @@
 def rev_str(string):
     rev1 = [i for i in reversed("".join([ch for ch in reversed(string)]).split(" "))]
     return " ".join(rev1)
+
 if __name__ == "__main__":
     string = input("Enter string : " )
     print(rev_str(string))
