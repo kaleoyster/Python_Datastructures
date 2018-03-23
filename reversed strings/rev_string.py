151a3f353a5490c8810655a8b70b6b428229d752
Akshay Kale <akshaykale915@gmail.com>
2018-03-22 20:10:29 -0500

    added reversed strings

---
+++ reversed strings/rev_string.py
@@ -0,0 +1,9 @@
+## Python program to reverse words in a string
+## Eg.  'Hello World' - > 'olleH dlroW'
+
+def rev_str(string):
+    rev1 = [i for i in reversed("".join([ch for ch in reversed(string)]).split(" "))]
+    return " ".join(rev1)
+if __name__ == "__main__":
+    string = input("Enter string : " )
+    print(rev_str(string))
