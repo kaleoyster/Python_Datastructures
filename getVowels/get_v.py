c391698c94367af59e66523b62bfcf0815230b07
Akshay Kale <akshaykale915@gmail.com>
2018-03-25 06:03:54 -0500

    added get vowel

---
+++ getVowels/get_v.py
@@ -0,0 +1,16 @@
+## Python Program to count vowels in a string
+
+
+
+def countVowels(string):
+    string = string.lower()
+    Vcount = 0 
+    vowels = {'a','e','i','o','u'}
+    for i in string:
+        if i in vowels:
+            Vcount = Vcount + 1
+    return Vcount
+
+if __name__=='__main__':
+    string = input("Enter String: ")
+    print(countVowels(string))
