f42914a279065fc2542cb4472e6133ba203e2060
Akshay Kale <akshaykale915@gmail.com>
2018-03-30 18:13:34 -0500

    attempt to take input to user

--- linkedlist/doublylinkedlist.py
+++ linkedlist/doublylinkedlist.py
@@ -1,3 +1,5 @@
+## Python Implementation of a doubly-linked list.
+
 class DoublyNode:
     def __init__(self, val):
         self.val = val
@@ -9,8 +11,14 @@ class DoublyNode:
         while node != None:
             print(node.val)
             node = node.next
-
+'''
 def main():
+    n = input("Enter the Length of Linked list: ")
+    print("Enter the numbers")
+    # Node is head
+    for index in range(0,len(n),1):
+        number = input()
+        node = DoublyNode(number)
     node1 = DoublyNode(1)
     node2 = DoublyNode(2)
     node3 = DoublyNode(3)
@@ -19,7 +27,18 @@ def main():
     node2.next = node3
 
     node1.traverse_front()
+'''
 
+def main():
+
+    node1 = DoublyNode(1)
+    node2 = DoublyNode(2)
+    node3 = DoublyNode(3)
+
+    node1.next = node2
+    node2.next = node3
+
+    node1.traverse_front()
 if __name__ == "__main__":
     main()
 
