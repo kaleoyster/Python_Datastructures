18556f66ea6cd474101c2bd4b4d3bde0b3b91f40
Akshay Kale <akshaykale915@gmail.com>
2018-04-01 23:57:20 -0500

    reverse string without special characters

---
+++ reverseArrayWOspChr/rev_arr.py
@@ -0,0 +1,37 @@
+
+
+
+def isAlphabet(string):
+    return string.isalpha()
+
+def reverse(string):
+    LIST  = toList(string)
+    r = len(LIST) - 1
+    l = 0
+
+
+    while l < r:
+        if not isAlphabet(LIST[l]):
+            l += 1
+        elif not isAlphabet(LIST[r]):
+            r -= 1
+        else: 
+            LIST[l], LIST[r] = swap(LIST[l], LIST[r])
+            l += 1
+            r -= 1
+
+    return toString(LIST)
+
+def toList(string):
+    return [i for i in string]
+
+def toString(List):
+    return ''.join(List)
+
+def swap(a,b):
+    return b, a
+
+
+string = 'a!!!b.c.d,ef,ghi'
+print(reverse(string))
+
