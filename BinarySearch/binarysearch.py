e7f847d5cf2f7283088e604178dfc0d0ee9c3dc2
Akshay Kale <akshaykale915@gmail.com>
2018-04-03 07:09:45 -0500

    added driver function

--- BinarySearch/binarysearch.py
+++ BinarySearch/binarysearch.py
@@ -2,7 +2,7 @@
 
 def bsearch(arr,l,r,s):
     if r >= l:
-        mid =int (l + (r-l)/2) 
+        mid =int (l + (r-l)/2)
         if arr[mid] == s:
             return mid
         elif arr[mid] > s:
@@ -12,11 +12,11 @@ def bsearch(arr,l,r,s):
     else:
         return -1
 
-
-arr = [2,2,3,4,5,6,10,40]
-s = 10
-result = bsearch(arr,0,len(arr)-1,s)
-if result != 1:
-    print(result)
-else:
-    print("No result")
+if __name__="__main__":
+    arr = [2,2,3,4,5,6,10,40]
+    s = 10
+    result = bsearch(arr,0,len(arr)-1,s)
+    if result != 1:
+        print(result)
+    else:
+        print("No result")
